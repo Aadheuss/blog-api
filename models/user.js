ac8cc4276a8742cac85e7bdd0e1c5e13b6de7483
Aadheuss <aadheusx@gmail.com>
2024-03-23 17:51:01 +0700

    Add new field and edit field on user schema

--- models/user.js
+++ models/user.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  username: { type: String, required: true, unique: true, maxLength: 60 },
+  password: { type: String, required: true, maxLength: 60 },
 });
 
 module.exports = mongoose.model("User", UserSchema);
