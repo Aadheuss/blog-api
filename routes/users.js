4b746906ba3b05cece65fb1ed0a1858b86f6a594
Aadheuss <aadheusx@gmail.com>
2024-03-27 01:58:58 +0700

    Change path to start with api and version

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,6 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
-
 router.post("/user", userController.user_sign_up);
 
 router.post("/user/login", userController.user_login);
