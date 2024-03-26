5686870bb626941565f9f3c18d7bf8e54f91d8f7
Aadheuss <aadheusx@gmail.com>
2024-03-26 23:28:32 +0700

    Add max length on title on post schema

--- models/post.js
+++ models/post.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  content: { type: String, minLength: 100, required: true },
+  title: { type: String, required: true, maxLength: 300 },
+  content: { type: String, required: true, minLength: 100 },
   published: { type: Boolean, required: true, default: false },
   time_stamp: { type: Date, required: true, default: Date.now },
 });
