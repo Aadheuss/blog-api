26e7ec52a9e55024c91730f79aa24c1d49cc1355
Aadheuss <aadheusx@gmail.com>
2024-03-29 08:01:56 +0700

    Change comment field name to text

--- models/comment.js
+++ models/comment.js
@@ -5,8 +5,17 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-  comment: { type: String, required: true },
+  text: { type: String, required: true },
   time_stamp: { type: Date, required: true, default: Date.now },
+  last_updated: { type: Date, required: true, default: Date.now },
+});
+
+CommentSchema.virtual("url").get(function () {
+  return `/comment/${this._id}`;
+});
+
+CommentSchema.virtual("edited").get(function () {
+  return this.time_stamp === this.last_updated;
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
